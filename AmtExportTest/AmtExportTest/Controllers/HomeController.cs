5b95825ae4edb6f5df5ccd1d4100b89b981558b9
Timofey S. Dubavik <tdubavik@modem.by>
2015-12-29 14:50:59 +0300

    main commit

---
+++ AmtExportTest/AmtExportTest/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using Modem.Amt.Export;
+using Modem.Amt.Export.Data;
+using Modem.Amt.Export.Connections;
+
+namespace AmtExportTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<JsonResult> GetData(long wellboreId, DateTime startTime, DateTime? endTime, List<Parameter> parameters)
+        {
+            Response.BufferOutput = false;
+
+            IRealtimeConnection testConnection;
+            testConnection = new TestConnection();
+
+            using (var store = new Store())
+            {
+                var data = store.GetData(parameters, new Wellbore { Id = wellboreId }, startTime, (DateTime)endTime);
+
+                Response.Write(data);
+                Response.Flush();
+            }
+
+            if (endTime == null)
+                return Json(new { success = true });
+            testConnection.ConfigureConnection(wellboreId, parameters);
+            while (true)
+            {
+                var newData = await testConnection.GetNewData();
+                if (newData == null) break;
+
+                Response.Write(newData);
+                Response.Flush();
+            }
+            Response.End();
+            return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetParameters()
+        {
+            using (var store = new Store())
+                return Json(store.Parameters.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetWellbores(DateTime startTime, DateTime? endTime)
+        {
+            using (var store = new Store())
+            {
+                var correctIntervals = new List<ContinuousInterval>();
+                
+                foreach (var wellbore in store.Wellbores)
+                {
+                    var interval = store.ContinuousIntervals.Where(x => x.WellboreId == wellbore.Id).SingleOrDefault();
+                    if (endTime == null) endTime = interval.StartTime;
+                    if (interval != null && endTime >= interval.StartTime && startTime <= interval.FinishTime)
+                        correctIntervals.Add(interval);
+                }
+                return Json(correctIntervals.Select(x => new
+                {
+                    wellboreId = x.WellboreId,
+                    wellboreStateId = store.WellboreStates.Any(y => y.WellboreId == x.WellboreId)
+                                      ? (long?)store.WellboreStates.Where(y => y.WellboreId == x.WellboreId).FirstOrDefault().StateId
+                                      : null,
+                    startTime = x.StartTime,
+                    endTime = x.FinishTime
+                })
+                    .ToList(), JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult GetWellboreStates()
+        {
+            using (var store = new Store())
+                return Json(store.WellboreStates.ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetUnits()
+        {
+            using (var store = new Store())
+                return Json(store.Units.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AmtExportTest/Modem.Amt.Export/Connections/TestConnection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modem.Amt.Export;
+using Modem.Amt.Export.Data;
+
+namespace Modem.Amt.Export.Connections
+{
+    public class TestConnection: IRealtimeConnection
+    {
+        private long wellboreId;
+        private List<Parameter> parameters;
+
+        public void ConfigureConnection(long wellboreId, List<Parameter> parameters) {
+            this.wellboreId = wellboreId;
+            this.parameters = parameters;
+        }
+        
+        public async System.Threading.Tasks.Task<decimal[]> GetNewData()
+        {
+            var random = new Random();
+            var randomData = await Task<decimal[]>.Run(() =>
+            {                
+                var randomArray = new decimal[5];
+                for (int i = 0; i < 5; i++) randomArray[i] = random.Next(0, 100);
+                return randomArray;
+            });
+            Task.WaitAll();
+
+            //var amtData = new AmtData();
+            //amtData.Data.Add(randomData);
+            if (random.Next(0, 10) == 9)
+                return null;
+            else
+                return randomData;
+        }
+    }
+}
